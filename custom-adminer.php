4b642252a1f8633a4326709d01e95713720ce590
Germain Italic <17506424+germain-italic@users.noreply.github.com>
2025-07-13 19:54:01 +0400

    Vérification complète de index.php

--- custom-adminer.php
+++ custom-adminer.php
@@ -1,19 +1,19 @@
 <?php
-// Plugin Adminer pour tri DESC par défaut
-class AdminerCustomSort extends AdminerPlugin {
-    function selectOrderProcess($fields, $indexes) {
-        $order = parent::selectOrderProcess($fields, $indexes);
-        
-        // Si aucun ordre spécifié, utilise la clé primaire en DESC
-        if (!$order) {
-            foreach ($indexes as $index) {
-                if ($index["type"] == "PRIMARY") {
-                    return array($index["columns"][0] => "DESC");
+namespace docker {
+    class AdminerCustomSort extends \Adminer\Plugin {
+        function selectOrderProcess($fields, $indexes) {
+            $order = parent::selectOrderProcess($fields, $indexes);
+            
+            // Si aucun ordre spécifié, utilise la clé primaire en DESC
+            if (!$order) {
+                foreach ($indexes as $index) {
+                    if ($index["type"] == "PRIMARY") {
+                        return array($index["columns"][0] => "DESC");
+                    }
                 }
             }
+            
+            return $order;
         }
-        
-        return $order;
     }
 }
-?>
